0f5cac149cfa28f0bd7e7b084cf474c17c3d1726
bahubba <bahubba89@gmail.com>
2023-06-10 22:47:04 -0400

    BAHubba-CU-85zt77hm9: Integrate with Spring Boot Auth
    WIP
    
    Added axios
    
    Added interfaces and properties files
    
    Added api folder and files for api calls

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,1 +1,6 @@
-export {};
+import axios, { CancelToken } from "axios";
+import { Registration } from "../interfaces";
+
+export const register = async (registration: Registration, cancelToken: CancelToken) => {
+
+}
---
+++ src/properties.ts
@@ -0,0 +1,7 @@
+const props = {
+    /* API Endpoints */
+    API_REGISTER_PATH: '/register',
+    API_LOGIN_PATH: '/login'
+};
+
+export default props;
